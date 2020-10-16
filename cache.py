9903009218c377233872550b7c91e1011c3037ba
KJTempas <fw6927mm@go.minneapolis.edu>
2020-10-16 09:06:20 -0500

    finish cache file

--- cache.py
+++ cache.py
@@ -10,19 +10,21 @@ from pymemcache import serde
 # cache is stored in binary
 #connect to memcached client
 memcache_client = Client('localhost', serde=serde.pickle_serde)
-#need 2 add methods ? one for hiking info an one for weather?
+"""
+memcache saves in key/value pairs
+these add and fetch  method will work for caching natlpark, hiking and weather info
+object is the data to be cached; add is a .set() and fetch is .get()
+identifier (the key) will be stateCode for natlparks, lat+long? for hiking, and date for weather
+expiry will vary by API; 1 month(2,628,000 sec)for natlparks and hiking and 1 day (86400) for weather
+need to type 'memcached' in terminal to activate memcache
+"""
+# in controller this fetch() will be called, and if the result is None, a new API call 
+#will be made; when the data is retrieved, add() will be called to set it in the cache
+def add(object, identifier, expiry): 
+    memcache_client.set(identifier, object, expire =expiry) 
 
-#def add_hiking(cachable):  #identifier is the key
-def add(object, identifier,   texpiry): 
-    memcache_client.set(identifier, object, expire = 1036000) 
-
-def add_weather(cachable):
-    memcache_client.set(cachable.identifier, cachable, expire = 36000)#3600sec = 1 hour
 
 def fetch(identifier, cls):
-    cached_object = memcache_client.get(identifier)
+    cached_object = memcache_client.get(identifier) #.get(key)
     return cached_object
 
-def fetch_weather(identifier, cls):
-    cached_object = memcache_client.get(identifier)
-    return cached_object
