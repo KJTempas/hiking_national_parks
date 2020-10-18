ca6599c2f7556620452adfb787d7948b8b856774
qian587 <tanxx587@umn.edu>
2020-10-18 13:50:51 -0400

    added back to home page

--- application/app.py
+++ application/app.py
@@ -6,7 +6,6 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(mes
 log = logging.getLogger('root')
 # from models import *
 app = Flask(__name__)
-# TODO need to validate if the parklist, weatherlist and traillist has data or no
 
 @app.route('/', methods=['GET'])
 def home():
@@ -19,25 +18,28 @@ def home():
 # TODO: Modified the route based on the selected state
 @app.route('/parks', methods=['GET'])
 def show_national_park():
-    # TODO Some state like CA will have out of range list
     state_input = request.args.get('states')
 
     park_list = natlParks_api.get_response(state_input.lower())
     return render_template('park_list.html', park_list=park_list, state=state_input)
 
 
-@app.route('/moreinfo/<park>/<lat>/<lon>')
+@app.route('/moreinfo/<park>/<lat>/<lon>', methods=['GET','POST'])
 def get_trail(park, lat, lon):
-    try:
+    if request.method == 'POST':
+        if request.form.get('trail'):
+            trail_obj = request.form.get('trail')
+            return '<h1>Here is the trail {}</h1>'.format(trail_obj)
+        elif request.form.get('back-page'):
+
+            return redirect(url_for('home'))
+    else:
         trail_list = hiking_api.get_trails(lat, lon)
         weather_list = weather_api.get_weather(lat, lon)
 
-
         return render_template('hikes_weather.html', park=park, trail_list=trail_list, weather_list=weather_list)
-    except Exception as e:
-        log.debug(e)
-        log.debug(lat)
-        log.debug(lon)
+
+
 
 
 if __name__ == "__main__":
