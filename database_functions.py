ab35379266c080e57232ca0e6deb009a38adfb9c
quinz <quinzel0325@gmail.com>
2020-10-18 13:39:18 -0500

    added db path & functions

---
+++ database_functions.py
@@ -0,0 +1,35 @@
+from peewee import *
+from models import Trails
+
+### use these functions to preform actions in the database
+
+def add_saved_trail(name, leng, dif, summ, natl_pk, state):
+    
+    Trails.create(trail_id = AutoField, trail_name = name, trail_len = leng, trail_difficulty = dif , trail_sum = summ, natl_park = natl_pk, date_saved = DateTimeField, state = state)
+    print(f'{name} has been added')
+
+
+def delete_trail_by_id(trail_id):
+
+    query = Trails.delete().where(Trails.trail_id == trail_id)
+    query.excute()
+
+def delete_everything():
+    """ Deletes all trails from database"""
+    Trails.delete().execute()
+
+
+def get_all_saved_trails():
+    """ Returns everything in db"""
+    query = Trails.select()
+    return list(query)
+
+def get_id_by_trail_name(name):
+
+    trail_id = Trails.get_id().where(Trails.trail_name = name)
+    return trail_id
+
+
+def saved_trail_count():
+    """ :returns the number of saved trails in the db """
+    return Trails.select().count()
--- models.py
+++ models.py
@@ -1,20 +1,23 @@
 from peewee import *
+from database_config import database_path
+import datetime
 
-db = SqliteDatabase(hiking_trails.db)
+db = SqliteDatabase(database_path)
 
 class Trails(Model):
 
-    # db id
+    ## db id
     trail_id = AutoField()
-    # hiking API returned items
+    ## hiking API returned items
     trail_name = CharField()
     trail_len = FloatField()
     trail_difficulty = CharField()
     trail_sum = CharField()
-    trail_img = CharField()
-    ##
+   # trail_img = CharField()
+    ## 
     natl_park = CharField()
-    date_saved = DateTimeField()
+    # will save the datetime of the moment it is saved
+    date_saved = DateTimeField(default=datetime.datetime.now)
     state = CharField()
     
 
