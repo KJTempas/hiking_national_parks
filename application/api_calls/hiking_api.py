59b1f74c097e85a03f15618cb914872a7a39bec1
qian587 <tanxx587@umn.edu>
2020-10-18 16:24:21 -0400

    resolve merge conflicts

--- application/api_calls/hiking_api.py
+++ application/api_calls/hiking_api.py
@@ -14,26 +14,33 @@ log = logging.getLogger('root')
 
 
 def get_trails(lat, lon):
-    try:
-        query = {'lat': lat, 'lon': lon, 'key': HIKING_KEY}
+    # see if trail_list is in cache; otherwise, do API call
+    # identifier is lat/long
+    if cached_trail_list := cache.fetch((lat, lon)):
+        log.info('Return from Cache')  # this will be deleted later
+        return cached_trail_list
+    else:
+        log.info('new API call')
+        try:
+            query = {'lat': lat, 'lon': lon, 'key': HIKING_KEY}
 
-        response = requests.get(HIKING_URL, params=query)
-        response.raise_for_status()  # will raise an exception for 400(client) or 500(server) errors
-        data = response.json()
-        trail_list = list()
-        list_of_trails = data['trails']
+            response = requests.get(HIKING_URL, params=query)
+            response.raise_for_status()  # will raise an exception for 400(client) or 500(server) errors
+            data = response.json()
+            trail_list = list()
+            list_of_trails = data['trails']
 
-        if list_of_trails:
-            for trail in list_of_trails:
-                trail_list_w_info = dict()
-                if trail['name'] and trail['length'] and trail['difficulty'] and trail['summary'] :
+            if list_of_trails:
+                for trail in list_of_trails:
+                    trail_list_w_info = dict()
+                    if trail['name'] and trail['length'] and trail['difficulty'] and trail['summary'] :
 
-                    trail_list_w_info['name'] = trail['name']
-                    trail_list_w_info['length'] = trail['length']
-                    trail_list_w_info['difficulty'] = trail['difficulty']
-                    trail_list_w_info['summary'] = trail['summary']
+                        trail_list_w_info['name'] = trail['name']
+                        trail_list_w_info['length'] = trail['length']
+                        trail_list_w_info['difficulty'] = trail['difficulty']
+                        trail_list_w_info['summary'] = trail['summary']
 
-                    trail_list.append(trail_list_w_info)
+                        trail_list.append(trail_list_w_info)
 
 
         # for x in range(-1, 5):
@@ -46,8 +53,9 @@ def get_trails(lat, lon):
         #     #
         # print(
         #     f'Name: {trail_name} | Trail summary: {trail_summary} | Trail length: {trail_length} | Trail difficulty: {trail_difficulty} | Trail img: {trail_img}')
-
-        return trail_list
-    except Exception as e:
-        log.exception(e)
-        raise e
+                        # add data to cache - expires in 1 month
+            cache.add((lat, lon), trail_list, 2628000)
+            return trail_list
+        except Exception as e:
+            log.exception(e)
+            raise e
