fe1fab806424ca9b0bc5a25ee671a4969c1af419
qian587 <tanxx587@umn.edu>
2020-10-25 21:50:19 -0400

    added logger to db functions

--- application/database/database_functions.py
+++ application/database/database_functions.py
@@ -1,18 +1,23 @@
 from peewee import *
 from .models import Trails
+import logging
 
-### use these functions to preform actions in the database
+# Logger
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%d-%m-%y %H:%M:%S')
+log = logging.getLogger('root')
+
+# use these functions to preform actions in the database
 
 def add_saved_trail(name, leng, summ, natl_pk, state):
-    
-    Trails.create(trail_id = AutoField, trail_name = name, trail_len = leng, trail_sum = summ, natl_park = natl_pk, date_saved = DateTimeField, state = state)
-    print(f'{name} has been added')
+    Trails.create(trail_id=AutoField, trail_name=name, trail_len=leng, trail_sum=summ, natl_park=natl_pk,
+                  date_saved=DateTimeField, state=state)
+    log.info(f'Added Trail: {name}  to the database.')
 
 
 def delete_trail_by_id(trail_id):
-
     query = Trails.delete().where(Trails.trail_id == trail_id)
     query.execute()
+
 
 def delete_everything():
     """ Deletes all trails from database"""
@@ -23,6 +28,7 @@ def get_all_saved_trails():
     """ Returns everything in db"""
     query = Trails.select()
     return list(query)
+
 
 # def get_id_by_trail_name(name):
 #
