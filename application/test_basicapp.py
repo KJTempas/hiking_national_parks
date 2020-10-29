4895e9d484754488e8a795b92e809d3e5302c44a
qian587 <tanxx587@umn.edu>
2020-10-29 11:38:43 -0400

    wip: testing parklist page

--- application/test_basicapp.py
+++ application/test_basicapp.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+from unittest.mock import patch
+
 from database import db_config
 from peewee import SqliteDatabase
 
@@ -33,8 +35,34 @@ class BasicTests(unittest.TestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    # mock the api call and provide a list of response that will be return
+    # TODO should change the return_value into something similar to the request response
+    @patch('app.natlParks_api.get_response', return_value=['Sequoia', 'Mojave'])
+    def test_state_page(self, get_response):
+        url = 'http://127.0.0.1:5000/parks?state=CA'
+        # assert list of parks on page
+        # make request
+        response = self.app.get(url)
+        print(response.data)
+        self.assertIn('Sequoia', get_response)
 
 
+    ###########
+    # Example #
+    ###########
+    # # Ensure that welcome page loads
+    # def test_welcome_route_works_as_expected(self):
+    #     response = self.client.get('/welcome', follow_redirects=True)
+    #     self.assertIn(b'Welcome to Flask!', response.data)
+    #
+    # # Ensure that posts show up on the main page
+    # def test_posts_show_up_on_main_page(self):
+    #     response = self.client.post(
+    #         '/login',
+    #         data=dict(username="admin", password="admin"),
+    #         follow_redirects=True
+    #     )
+    #     self.assertIn(b'This is a test. Only a test.', response.data)
 
 if __name__ == "__main__":
     unittest.main()
